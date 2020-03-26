f21a4692bdb37f75e5931c006e0197975f00e50c
AndrewLester <alester220@gmail.com>
2020-03-25 23:35:29 -0400

    Reduce SQLAlchemy reconnect

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_RECONNECT = 3000
+    SQLALCHEMY_RECONNECT = 1000
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
