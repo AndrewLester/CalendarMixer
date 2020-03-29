7b5dc0e7d5c63f1fee4e00e18bfb2979205ed261
AndrewLester <alester220@gmail.com>
2020-03-28 22:54:59 -0400

    Reduce connection recycle time

--- config.py
+++ config.py
@@ -11,7 +11,8 @@ class Config:
     # ClearDB's idle limit is 90 seconds, so set the recycle to be under 90
     if os.environ.get('DATABASE_URL'):
         SQLALCHEMY_POOL_SIZE = 5
-        SQLALCHEMY_POOL_RECYCLE = 80
+        SQLALCHEMY_POOL_RECYCLE = 75
+        SQLALCHEMY_POOL_TIMEOUT = 10
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
