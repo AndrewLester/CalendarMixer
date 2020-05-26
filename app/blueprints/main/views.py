4c52734e1865e1e425d0d0e7b9652f885a861bcb
AndrewLester <alester220@gmail.com>
2020-05-26 01:11:42 -0400

    Don't cache pages using user data

--- app/blueprints/main/views.py
+++ app/blueprints/main/views.py
@@ -7,7 +7,6 @@ blueprint = Blueprint('main', __name__, template_folder='../../templates', stati
 
 
 @blueprint.route('/')
-@cache_header(1800)
 def index():
     return render_template('index.html', title='Home')
 
