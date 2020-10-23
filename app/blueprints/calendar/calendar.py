f8a611d453e87a5dd381360ef16970b04e479dc2
AndrewLester <alester220@gmail.com>
2020-10-23 10:05:01 -0400

    Use seconds rather than microseconds to fix google script error

--- app/blueprints/calendar/calendar.py
+++ app/blueprints/calendar/calendar.py
@@ -57,9 +57,9 @@ class SchoologyCalendar:
         for alert in self.alerts:
             # Invert timedelta to make it represent time before the event
             inverted_timedelta = -alert.timedelta
-            # Alarms at the event's time break, so use the event's time - 1 microsecond
+            # Alarms at the event's time break, so use the event's time - 1 second
             if not inverted_timedelta:
-                inverted_timedelta = timedelta(microseconds=-1)
+                inverted_timedelta = timedelta(seconds=-1)
 
             if alert.type == EventAlertType.EMAIL:
                 alarm = EmailAlarm(inverted_timedelta)
